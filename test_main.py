e691d2cdc1f52e94dfd08c36808b54e62061325c
Pitickozdravicko <91801560+Pitickozdravicko@users.noreply.github.com>
2023-01-17 15:48:15 +0100

    Add files via upload

--- test_main.py
+++ test_main.py
@@ -2,17 +2,26 @@ import main
 import pytest
 from io import StringIO
 import sys
+"""
+@author: psantler
+"""
 
 numbers_input = [1, 5, 9, 6, 3, 8, 2, 7, 4]
 numbers_input_pseudorandom = main.generate_pseoudorandom_array(main.ARRAY_LEGTH, main.GROUND_VALUE, main.TOP_VALUE)
 
 
 def test_input_file(get_file_numbers):
-
+    """
+    test kontrolující správnost zadaných dat v souboru
+    :param get_file_numbers: soubor s testovacími daty
+    """
     assert get_file_numbers == [1, 4, 5, 6, 8, 3, 9, 7, 2]
 
 
 def test_generate_pseudorandom():
+    """
+    Test kontrolující generování náhodných čísel
+    """
     x = 0
     while x < 100:
         assert numbers_input_pseudorandom != main.generate_pseoudorandom_array(main.ARRAY_LEGTH,
@@ -21,6 +30,13 @@ def test_generate_pseudorandom():
 
 
 def test_bubble_sort(get_file_numbers):
+    """
+    Test kontrolující funkčnost bubbble sortu, pro kontrolu používá pevnou sadu testovacích dat, soubor s předvolenou testovací sadou a nádohně generovanou testovací sadu
+    :param get_file_numbers: soubor s testovacími daty
+    """
+    list_numbers_input_pseudorandom = sorted(numbers_input_pseudorandom)
+    list_merged = main.bubble_sort(numbers_input_pseudorandom)
+    assert list_merged == list_numbers_input_pseudorandom
 
     list_merged = main.bubble_sort(numbers_input)
     assert list_merged == [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -30,6 +46,13 @@ def test_bubble_sort(get_file_numbers):
 
 
 def test_merge_sort(get_file_numbers):
+    """
+    Test kontrolující funkčnost merge sortu, pro kontrolu používá pevnou sadu testovacích dat, soubor s předvolenou testovací sadou a nádohně generovanou testovací sadu
+    :param get_file_numbers:  soubor s testovacími daty
+    """
+    list_numbers_input_pseudorandom = sorted(numbers_input_pseudorandom)
+    list_merged = main.merge_sort(numbers_input_pseudorandom)
+    assert list_merged == list_numbers_input_pseudorandom
 
     list_merged = main.merge_sort(numbers_input)
     assert list_merged == [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -39,6 +62,14 @@ def test_merge_sort(get_file_numbers):
 
 
 def test_insertion_sort(get_file_numbers):
+    """
+    Test kontrolující funkčnost insertion sortu, pro kontrolu používá pevnou sadu testovacích dat, soubor s předvolenou testovací sadou a nádohně generovanou testovací sadu
+    :param get_file_numbers:
+    """
+
+    list_numbers_input_pseudorandom = sorted(numbers_input_pseudorandom)
+    list_merged = main.insertion_sort(numbers_input_pseudorandom)
+    assert list_merged == list_numbers_input_pseudorandom
 
     list_merged = main.insertion_sort(numbers_input)
     assert list_merged == [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -48,14 +79,26 @@ def test_insertion_sort(get_file_numbers):
 
 
 def test_lowest_num(sort_numbers):
+    """
+    Test kontrolující funkčnost hledání nejmenšího čísla
+    :param sort_numbers: seřazená čisla
+    """
     assert main.get_lowest_num(sort_numbers) == 1
 
 
 def test_highest_num(sort_numbers):
+    """
+    Test kontrolující funkčnost hledání největšího čísla
+    :param sort_numbers: seřazená čísla
+    """
     assert main.get_highest_num(sort_numbers) == 9
 
 
 def test_sort_numbers(monkeypatch):
+    """
+    Test kontrolující funkčnost výběru typu sortu, nejprve proběhne inicializace user inputu, poté jeho simulace, nakonec se spustí samotná funkce se simulovaným inputem
+    :param monkeypatch: monkeypatch simuluje user input v průběhu testu
+    """
     number_inputs = StringIO('1')
     monkeypatch.setattr('sys.stdin', number_inputs)
     output = main.sort_numbers(numbers_input)
@@ -75,12 +118,13 @@ def test_sort_numbers(monkeypatch):
     assert output == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def test_main(monkeypatch):
-    assert True
-
-
 @pytest.fixture
 def get_file_numbers(monkeypatch):
+    """
+    fixture díky níž načítáme data ze souboru
+    :param monkeypatch: monkeypatch simuluje user input v průběhu testu
+    :return:
+    """
     numbers_inputs = StringIO("numbers.txt")
     monkeypatch.setattr("sys.stdin", numbers_inputs)
     filename = input("get file")
@@ -91,4 +135,9 @@ def get_file_numbers(monkeypatch):
 
 @pytest.fixture
 def sort_numbers(get_file_numbers):
+    """
+    fixture díky níž sortujeme čísla pomocí předem definované sort funkce jazyka python pro kontrolu v dalších testech
+    :param get_file_numbers:
+    :return:
+    """
     return sorted(get_file_numbers)
